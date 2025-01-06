757e4f19b5333e6456439e195da7adcabce2b0b5
thuvaragan-dm <thuvaragan@deepmodel.ai>
2025-01-06 20:55:44 +0530

    Initial commit.

--- src/app/optimum-space/page.tsx
+++ src/app/optimum-space/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { useCallback, useEffect, useRef, useState } from "react";
+import * as XLSX from "xlsx";
 import { Rectangle } from "./arrange"; // Ensure arrangeRectangles is used appropriately
 import Population from "./Population"; // Ensure you import your Population class
 import DNA from "./DNA";
@@ -16,25 +17,23 @@ const Page = () => {
   const [answer, setAnswer] = useState<DNA | null>(null);
 
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  const maxGenerations = 500; // Set the maximum number of generations
-  const mutationRate = 0.01; // Set mutation rate
-  const maximumPopulation = 1500; // Set the size of the population
+  const maxGenerations = 500;
+  const mutationRate = 0.01;
+  const maximumPopulation = 1500;
 
   const [rectangles, setRectangles] = useState<Rectangle[]>([]);
 
   const calculate = useCallback(() => {
     if (rectangles.length <= 0) return;
-    // Create the initial population
     const population = new Population(
       mutationRate,
       maximumPopulation,
       rectangles
     );
 
-    // Run the genetic algorithm for a number of generations
     for (let i = 0; i < maxGenerations; i++) {
       population.naturalSelection();
-      population.generate(); // Pass the available rectangles to generate
+      population.generate();
       population.evaluate();
       displayInfo();
       setResults((pv) => [
@@ -48,12 +47,10 @@ const Page = () => {
       ]);
     }
 
-    // Get the best layout after running the algorithm
     const bestDNA = population.getBest();
     setAnswer(bestDNA);
     console.log({ bestDNA });
 
-    // Setup the canvas to draw the best arrangement
     const canvas = canvasRef.current;
 
     function displayInfo() {
@@ -66,23 +63,18 @@ const Page = () => {
     }
 
     if (canvas && bestDNA) {
-      // Ensure bestDNA is defined
       const context = canvas.getContext("2d");
       if (!context) return;
 
-      // Set canvas dimensions based on the best arrangement
       canvas.width = bestDNA.genes.containerWidth;
       canvas.height = bestDNA.genes.containerHeight;
 
-      // Clear canvas
       context.clearRect(0, 0, canvas.width, canvas.height);
 
-      // Draw each rectangle in the best arrangement
       bestDNA.genes.positionedRectangles.forEach((rect) => {
-        context.fillStyle = "lightblue"; // Set color for the rectangle
-        context.fillRect(rect.x, rect.y, rect.width, rect.height); // Draw rectangle
+        context.fillStyle = "lightblue";
+        context.fillRect(rect.x, rect.y, rect.width, rect.height);
 
-        // Optional: Draw rectangle border
         context.strokeStyle = "black";
         context.strokeRect(rect.x, rect.y, rect.width, rect.height);
       });
@@ -96,6 +88,40 @@ const Page = () => {
   const widthRef = useRef<HTMLInputElement>(null);
   const heightRef = useRef<HTMLInputElement>(null);
 
+  // Function to handle file upload
+  const handleFileUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      const data = new Uint8Array(event.target?.result as ArrayBuffer);
+      const workbook = XLSX.read(data, { type: "array" });
+
+      // Assuming the first sheet contains the data
+      const sheet = workbook.Sheets[workbook.SheetNames[0]];
+      const rows = XLSX.utils.sheet_to_json<[number, number]>(sheet, {
+        header: 1,
+      });
+
+      // Parse rows to create rectangles
+      const newRectangles: Rectangle[] = [];
+      rows.forEach((row, index) => {
+        // Skip header row if present
+        if (index === 0) return;
+
+        const [width, height] = row;
+        newRectangles.push({ width, height });
+      });
+
+      console.log({ newRectangles });
+
+      setRectangles(newRectangles);
+    };
+
+    reader.readAsArrayBuffer(file);
+  };
+
   return (
     <div className="w-full h-dvh flex items-start justify-between">
       {/* canvas */}
@@ -108,7 +134,7 @@ const Page = () => {
       <div className="w-[50%] h-dvh p-2 flex flex-col">
         <div className="w-full flex flex-col flex-1 overflow-y-auto p-5 rounded-xl bg-gradient-to-br from-gray-900 to-gray-950">
           <header className="w-full flex items-center justify-between gap-10">
-            <div className="">
+            <div>
               <h1 className="text-2xl font-semibold text-white">
                 Genetic Results
               </h1>
@@ -125,87 +151,42 @@ const Page = () => {
             </button>
           </header>
 
-          <div className="mt-5">
-            <div className="w-full flex items-center justify-between gap-10">
-              <div className="w-max text-white">
-                <h1 className="text-2xl font-semibold text-white">Add boxes</h1>
-                <p className="text-xs text-white/50">
-                  You can add / delete boxes
-                </p>
-              </div>
+          <div className="mt-5 flex justify-end flex-col items-end">
+            <h2 className="text-white text-xs">Load Rectangles from Excel</h2>
+            <input
+              id="file"
+              type="file"
+              onChange={handleFileUpload}
+              className="mt-2 p-2 sr-only rounded-md"
+            />
 
-              <form
-                onSubmit={(e) => {
-                  e.preventDefault();
-                  const { value: width } = widthRef.current || {};
-                  const { value: height } = heightRef.current || {};
+            <button
+              onClick={() => document.getElementById("file")?.click()}
+              className="mt-1 p-3 rounded-xl bg-white text-gray-800 font-medium text-sm hover:bg-white/90 active:scale-95 transition-all duration-300"
+            >
+              Load
+            </button>
+          </div>
 
-                  // Validate width and height together in a single condition
-                  if (!width || !height || isNaN(+width) || isNaN(+height))
-                    return;
-
-                  // Parse width and height once after validation
-                  const w = parseFloat(width);
-                  const h = parseFloat(height);
-
-                  // Create and update rectangle list
-                  const rect: Rectangle = { width: w, height: h };
-                  setRectangles((prev) => [...prev, rect]);
-
-                  if (widthRef.current?.value) {
-                    widthRef.current.value = "";
-                  }
-
-                  if (heightRef.current?.value) {
-                    heightRef.current.value = "";
-                  }
-                }}
-                className="flex items-center justify-end gap-3"
-              >
-                <input
-                  ref={widthRef}
-                  type="number"
-                  placeholder="Enter width"
-                  className="px-3 w-32 py-2 bg-transparent border outline-none border-white/60 text-white rounded-xl focus:outline-none focus:ring-2 ring-offset-1 ring-offset-gray-900 focus:ring-white/10"
-                />
-
-                <input
-                  ref={heightRef}
-                  type="number"
-                  placeholder="Enter height"
-                  className="px-3 w-32 py-2 bg-transparent border outline-none border-white/60 text-white rounded-xl focus:outline-none focus:ring-2 ring-offset-1 ring-offset-gray-900 focus:ring-white/10"
-                />
-
-                <button
-                  type="submit"
-                  className="p-3 rounded-xl bg-white text-gray-800 font-medium text-sm hover:bg-white/90 active:scale-95 transition-all duration-300"
-                >
-                  Add
-                </button>
-              </form>
-            </div>
-
-            {/* boxes */}
-            <div className="mt-5 w-full flex items-center justify-start gap-5 overflow-x-auto font-mono">
-              {rectangles.map((rectangle, index) => (
-                <div className="bg-white/10 rounded-xl p-3 text-white font-mono text-xs">
-                  <pre>{JSON.stringify(rectangle, null, 2)}</pre>
-
-                  <div className="mt-2 flex items-end justify-end">
-                    <button
-                      onClick={() => {
-                        setRectangles((pv) =>
-                          pv.filter((_, idx) => idx !== index)
-                        );
-                      }}
-                      className="text-xs text-white hover:underline"
-                    >
-                      delete
-                    </button>
-                  </div>
+          {/* Display rectangles */}
+          <div className="mt-5 w-full flex items-center justify-start gap-5 overflow-x-auto font-mono">
+            {rectangles.map((rectangle, index) => (
+              <div className="bg-white/10 rounded-xl p-3 text-white font-mono text-xs">
+                <pre>{JSON.stringify(rectangle, null, 2)}</pre>
+                <div className="mt-2 flex items-end justify-end">
+                  <button
+                    onClick={() => {
+                      setRectangles((pv) =>
+                        pv.filter((_, idx) => idx !== index)
+                      );
+                    }}
+                    className="text-xs text-white hover:underline"
+                  >
+                    delete
+                  </button>
                 </div>
-              ))}
-            </div>
+              </div>
+            ))}
           </div>
 
           {answer && (
@@ -278,7 +259,6 @@ const Page = () => {
           </div>
         </div>
       </div>
-      {/* results */}
     </div>
   );
 };
